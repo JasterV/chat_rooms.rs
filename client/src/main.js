7704f8f1a6068273305c812a1cc6343c208c1613
JasterV <victorcoder2@gmail.com>
2020-11-08 19:33:17 +0100

    client front end in progress

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,22 @@
-require('main.css');
+require('./main.css');
 const net = require('net');
+const $ = require('jquery');
+
+$(() => {
+
+    $('#join-form').on('submit', (e) => {
+        e.preventDefault();
+    })
+
+    $('#join-btn').on('click', () => {
+        $("#choose-room, #join-section").toggle();
+    })
+
+    $("#back-btn").on('click', () => {
+        $("#choose-room, #join-section").toggle();
+    });
+
+});
 
 // const client = new net.Socket();
 // connector.connect(PORT, HOST, function () {
